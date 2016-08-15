d892fd141e4a7de2a9ac28bd420c4917af459775
Iris Yang <c3b841121@gmail.com>
2016-08-15 17:28:02 +0800

    Modify server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require("express");
+var http = require("http");
+var path = require("path");
 var app = express();
 // for static files
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get( '/' , function(req, res){
+    res.sendFile("index.html", { root: __dirname });
+});
 var server = http.createServer(app);
 server.listen(process.env.PORT || 8080,function(){
     console.log("Server running...");
